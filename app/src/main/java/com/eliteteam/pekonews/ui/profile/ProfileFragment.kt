d499ce0c54354d5823407958130e692a2d095923
Faiz <faizadli9912@gmail.com>
2021-12-08 21:07:48 +0700

    Update Profile

--- app/src/main/java/com/eliteteam/pekonews/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/eliteteam/pekonews/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.eliteteam.pekonews.ui.dashboard
+package com.eliteteam.pekonews.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.eliteteam.pekonews.R
-import com.eliteteam.pekonews.databinding.FragmentDashboardBinding
+import com.eliteteam.pekonews.databinding.FragmentProfileBinding
 
-class DashboardFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private lateinit var profileViewModel: ProfileViewModel
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +24,10 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        profileViewModel =
+            ViewModelProvider(this)[ProfileViewModel::class.java]
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
         return binding.root
     }
